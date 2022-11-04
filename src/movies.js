f1aaba79440ce4156c49d8dfb807a4515cae8419
Alejandro Arends <alejandroarends77@gmail.com>
2022-11-04 10:56:50 +0100

    Lab Greatest Movies of All Time: 1st Iteration + Bonus

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,30 @@
 // Iteration 1: All directors? - Get the array of all directors.
+
+// function getAllDirectors (moviesArray){
+//     return moviesArray.map((moviesArray) => moviesArray.director); 
+// }
+
+let directorsMap = function getAllDirectors (pepe){
+    return pepe.map((el) => el.director);
+};
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+
+let directorsMapArr = new map(directorsMap);
+
+let unicos = [...directorsMapArr.values()];
+return unicos;
+
+
+// function getAllDirectors(moviesArray) {}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+// function howManyMovies(moviesArray) {}
+function howManyMovies(pepe) {
+    
+}
+
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {}
